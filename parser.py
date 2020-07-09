5d1f84755640bda6e43cd3f52c7011cc0a010c14
Vardhan Agnihotri <60634457+UltraScienceGuy@users.noreply.github.com>
2020-07-09 11:42:30 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,15 +1,15 @@
 import imaplib, re
 import email
 from email.header import decode_header
+import playsound  # to play saved mp3 file
+from gtts import gTTS  # google text to speech
+from pynput.keyboard import Key, Controller, Listener
+import os
 
-pattern_uid = re.compile('\d+ \(UID (?P<uid>\d+)\)')
+keyboard = Controller()
 
-def parse_uid(data):
-    match = pattern_uid.match(data)
-    return match.group('uid')
-
-username = "my email"
-password = "my password"
+username = "va648314@gmail.com"
+password = "stmmsp3214"
 
 imap = imaplib.IMAP4_SSL("imap.gmail.com")
 
@@ -17,28 +17,62 @@ imap.login(username, password)
 
 status, messages = imap.select("INBOX")
 
-N = 6
+N = 1
 
 messages = int(messages[0])
+num = 1
+
+def delete_message():
+    imap.select(mailbox='Inbox', readonly=False)
+    resp, items = imap.search(None, 'All')
+    email_ids = items[0].split()
+    latest_email_id = email_ids[-1]  # Assuming that you are moving the latest email.
+
+    resp, data = imap.fetch(latest_email_id, "(UID)")
+    msg_uid = parse_uid(data[0])
+
+    result = imap.uid('COPY', msg_uid, 'Trash')
+
+    if result[0] == 'OK':
+        mov, data = imap.uid('STORE', msg_uid, '+FLAGS', '(\Deleted)')
+        imap.expunge()
 
 
+def assistant_speaks(output):
+    global num
 
-for i in range(messages, messages-N, -1):
-    data, msg = imap.fetch(str(i), "(RFC822)")
-    for response in msg:
-        if isinstance(response, tuple):
-            msg = email.message_from_bytes(response[1])
+    # num to rename every audio file
+    # with different name to remove ambiguity
+    num += 1
+    print("Alaska : ", output)
 
-            subject = decode_header(msg["Subject"])[0][0]
-            if isinstance(subject, bytes):
+    toSpeak = gTTS(text=output, lang='en', slow=False)
+    # saving the audio file given by google text to speech
+    file = str(num) + ".mp3"
+    toSpeak.save(file)
 
-                subject = subject.decode()
-            print("=" * 100)
-            from_ = msg.get("From")
-            print("Subject:", subject)
-            print("From:", from_)
-            print("=" * 100)
-            if "YouTube" in from_:
+    # playsound package is used to play the same file.
+    playsound.playsound(file, True)
+    os.remove(file)
+
+def driver_code():
+    for i in range(messages, messages-N, -1):
+        data, msg = imap.fetch(str(i), "(RFC822)")
+        for response in msg:
+            if isinstance(response, tuple):
+                msg = email.message_from_bytes(response[1])
+
+                subject = decode_header(msg["Subject"])[0][0]
+                if isinstance(subject, bytes):
+
+                    subject = subject.decode()
+                print("=" * 100)
+                from_ = msg.get("From")
+                print("Subject:", subject)
+                assistant_speaks("subject is " + subject)
+                print("From:", from_)
+                assistant_speaks("From " + from_)
+                print("=" * 100)
                 if msg.is_multipart():
                     for part in msg.walk():
                         content_type = part.get_content_type()
@@ -53,12 +87,18 @@ for i in range(messages, messages-N, -1):
                             print(body)
                             print("=" * 100)
 
-                    msg_uid = parse_uid(data[0])
-                    result = imap.uid('COPY', msg_uid, 'Trash')
+    imap.close()
+    imap.logout()
 
-                    if result[0] == 'OK':
-                        mov, data = imap.uid('STORE', msg_uid, '+FLAGS', '(\Deleted)')
-                        imap.expunge()
+if __name__ == '__main__':
+    try:
+        driver_code()
+        yoink = input("Would you like to delete the message (y/n)? ")
+        if yoink == "y":
+            try:
+                delete_message()
+            except:
+                assistant_speaks("An error occurred, please try again")
+    except:
+        assistant_speaks("An error occurred, please try again")
 
-imap.close()
-imap.logout()
