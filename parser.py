5a3515f85f7f809202974ad83557c7f6248d7c62
Vardhan Agnihotri <60634457+UltraScienceGuy@users.noreply.github.com>
2020-07-09 10:48:40 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,9 +1,15 @@
-import imaplib
+import imaplib, re
 import email
 from email.header import decode_header
 
-username = "your username"
-password = "your password"
+pattern_uid = re.compile('\d+ \(UID (?P<uid>\d+)\)')
+
+def parse_uid(data):
+    match = pattern_uid.match(data)
+    return match.group('uid')
+
+username = "my email"
+password = "my password"
 
 imap = imaplib.IMAP4_SSL("imap.gmail.com")
 
@@ -18,7 +24,7 @@ messages = int(messages[0])
 
 
 for i in range(messages, messages-N, -1):
-    res, msg = imap.fetch(str(i), "(RFC822)")
+    data, msg = imap.fetch(str(i), "(RFC822)")
     for response in msg:
         if isinstance(response, tuple):
             msg = email.message_from_bytes(response[1])
@@ -27,30 +33,32 @@ for i in range(messages, messages-N, -1):
             if isinstance(subject, bytes):
 
                 subject = subject.decode()
-
+            print("=" * 100)
             from_ = msg.get("From")
             print("Subject:", subject)
             print("From:", from_)
-            if "Discord" in from_:
-                imap.expunge()
-            # if discord spams me, then I am able to delete the message
-            if msg.is_multipart():
-                for part in msg.walk():
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get("Content-Disposition"))
-                    try:
-                        # get the email body
-                        body = part.get_payload(decode=True).decode()
-                    except:
-                        pass
-                    if content_type == "text/plain" and "attachment" not in content_disposition:
-                        # print text/plain emails and skip attachments
-                        print(body)
-                        print("=" * 100)
-            else:
-                content_type = msg.get_content_type()
-                body = msg.get_payload(decode=True).decode()
-                if content_type == "text/plain":
-                    print(body)
+            print("=" * 100)
+            if "YouTube" in from_:
+                if msg.is_multipart():
+                    for part in msg.walk():
+                        content_type = part.get_content_type()
+                        content_disposition = str(part.get("Content-Disposition"))
+                        try:
+                            # get the email body
+                            body = part.get_payload(decode=True).decode()
+                        except:
+                            pass
+                        if content_type == "text/plain" and "attachment" not in content_disposition:
+                            # print text/plain emails and skip attachments
+                            print(body)
+                            print("=" * 100)
+
+                    msg_uid = parse_uid(data[0])
+                    result = imap.uid('COPY', msg_uid, 'Trash')
+
+                    if result[0] == 'OK':
+                        mov, data = imap.uid('STORE', msg_uid, '+FLAGS', '(\Deleted)')
+                        imap.expunge()
+
 imap.close()
 imap.logout()
